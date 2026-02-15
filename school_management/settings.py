9858f6337574dbe79cad4555e00ea1300cd78d26
zainchodry <razzaqzain981@gmail.com>
2026-02-16 00:48:13 +0500

    Added minor changes for email send notifications

--- school_management/settings.py
+++ school_management/settings.py
@@ -168,3 +168,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
